36c2245b4c5a6a18fa0af47a2763e7e96d5d76c0
Amruta Kulkarni <74787831+Amrutakulkarni-07@users.noreply.github.com>
2021-03-14 20:12:28 +0530

    linked list in java

---
+++ simple_linked_list/Demo.java
@@ -0,0 +1,249 @@
+
+//java code for linked list 
+
+import java.lang.*;
+import java.util.*;
+class Node
+{
+    public int data;
+    public Node next;
+    public Node(int value)
+    {
+        data = value;
+        next = null;
+    }
+};
+class SinglyLinkedList
+{
+    public Node Head;
+    public SinglyLinkedList()
+    {
+        Head = null;
+    }
+    public void InsertFirst(int no)
+    {
+        Node newn = new Node(no);    // Kamal statement ahe
+        if(Head == null)
+        {
+            Head = newn;
+        }
+        else
+        {
+            newn.next = Head;
+            Head = newn;
+        }
+    }
+    public void InsertLast(int no)
+    {
+        Node newn = new Node(no);    // Kamal statement ahe
+        if(Head == null)
+        {
+            Head = newn;
+        }
+        else
+        {
+            Node temp = Head;
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = newn;
+        }
+    }
+    public void Display()
+    {
+        Node temp = Head;
+        System.out.println("Elements of linked list are: ");
+        
+        while(temp != null)
+        {
+            System.out.print("|"+temp.data+"|->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public int Count()
+    {
+        int icnt = 0;
+        Node temp = Head;
+        while(temp != null)
+        {
+			icnt++;
+            temp = temp.next;
+        }
+		return icnt;
+    }
+	void DeleteFirst()
+	{
+		if(Head==null)     //empty 
+		{
+			return ;
+		}
+		else       //one or more node 
+		{
+			Head=Head.next;
+		}
+	}
+	public void DeleteLast()
+	{
+		if(Head==null)
+		{
+			return ;
+		}
+		else
+		{
+			if(Head.next==null)   //single code 
+			{
+				Head=null;
+				return;
+			}
+			Node temp=Head;
+			while(temp.next.next!=null)
+			{
+				temp=temp.next;
+			}
+			
+			temp.next=null;
+		}
+	}
+	
+	public void InsertAtPos(int pos,int no)
+	{
+		int i=0;
+		int size=Count();
+		
+		if((pos<1)|| (pos>(size+1)))
+       {
+           return;
+       }
+		if(pos==1)
+		{
+			InsertFirst(no);
+		}
+		else if(pos== (size+1))    
+		{
+			InsertLast(no);
+		}
+		else 
+		{
+			Node newn=new Node(no);
+			
+			Node temp=Head;
+			for(i=1;i<pos-1;i++)
+			{
+				temp=temp.next;
+			}
+			newn.next=temp.next;
+			temp.next=newn;
+		}
+	}
+	
+	public void DeleteAtPos(int pos)
+	{
+		int i=0;
+		int size=Count();
+		
+		if((pos<=0)|| (pos>size))
+       {
+           return;
+       }
+		if(pos==1)
+		{
+			DeleteFirst();
+		}
+		else if(pos==size)
+		{
+			DeleteLast();
+		}
+		else 
+		{
+			
+			Node temp=Head;
+			for(i=1;i<pos-1;i++)
+			{
+				temp=temp.next;
+			}
+			temp.next=temp.next.next;
+		}
+	}
+} ;
+    
+class Demo
+{
+    public static void main(String arg[])
+    {
+		  SinglyLinkedList obj = new SinglyLinkedList();
+		Scanner sobj=new Scanner(System.in);
+		
+		int choice=1;
+		int pos=0;
+		System.out.println("inside amruta collection framework");
+		while(choice!=0)
+		{
+			System.out.println("--------------------");
+		System.out.println("enter your choice:");
+		System.out.println("--------------------");
+		System.out.println("1.insert first:");
+		System.out.println("2.insert last:");
+		System.out.println("3.delete first:");
+		System.out.println("4.delete last:");
+		System.out.println("5.display");
+		System.out.println("6.count");
+		System.out.println("7.insert at pos");
+		System.out.println("8.delete at pos");
+	   System.out.println("0.exit:");
+	   
+		System.out.println("--------------------");
+        choice=sobj.nextInt();
+		switch(choice)
+		{
+      
+           case 1:
+		   System.out.println("enter data");
+		   int data=sobj.nextInt();
+		   obj.InsertFirst(data);
+		   break;
+		   
+		   case 2:
+		   System.out.println("enter data");
+		   int data1=sobj.nextInt();
+		   obj.InsertLast(data1);
+		   break;
+        
+		  case 3:
+		   obj.DeleteFirst();
+		   break;
+		   
+		   case 4:
+		   obj.DeleteLast();
+		   break;
+		   
+            case 5:
+			obj.Display();
+			break;
+			
+			case 6:
+			System.out.println("Number of nodes are : "+obj.Count());
+			break;
+			
+			case 7:
+		   System.out.println("enter data");
+		   int data3=sobj.nextInt();
+		    System.out.println("enter position");
+		    pos=sobj.nextInt();
+		   obj.InsertAtPos(pos,data3);
+		   break;
+		   case 8:
+		    System.out.println("enter position");
+		   int pos1=sobj.nextInt();
+		   obj.DeleteAtPos(pos1);
+		   break;
+		   
+		   
+         }  //switch end 
+        }  //while end 
+		
+		System.out.println("thank you for using our linked list ");
+		obj.Head=null;
+}
+}
