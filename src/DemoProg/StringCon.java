be89c9e2a6fede6b1f8518d4c42a97642c3516b4
amruta <amrutakulkarni808@gmail.com>
2025-09-20 22:11:34 +0530

    string concat

--- src/DemoProg/StringCon.java
+++ src/DemoProg/StringCon.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class StringCon {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		// TODO Auto-generated method stub
 		
